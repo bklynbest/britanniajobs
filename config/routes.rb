fa3d9e514a51dc785e57cc1c4e657d5895050b25
Jonathan Momperousse <bklynbest718@gmail.com>
2018-09-13 17:59:28 +0000

    updated views for job form, new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :jobs
   devise_for :users
-  root  'home#index'
+  root to:  'jobs#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
