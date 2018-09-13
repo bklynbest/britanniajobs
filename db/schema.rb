524f94eae55fd8102df29ec9bdf350b63318a40e
Jonathan Momperousse <bklynbest718@gmail.com>
2018-09-13 17:23:26 +0000

    added user id foreign key to jobs, and configured stripe for use in app

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180910190347) do
+ActiveRecord::Schema.define(version: 20180913172138) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20180910190347) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "avatar"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
